fbf53a04ec32ecfade42e4c52d62fae4dacbc814
nattec <natalia.cornejo@pucp.pe>
2021-08-29 15:27:19 +0200

    Create a calendar buttom #3

---
+++ App/todoapp/lib/models/global.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color greyColor= new Color(0xFF707070);
+Color blueColor= new Color(0xFF3793D9);
+
+TextStyle profileNameStyle = new TextStyle (fontFamily: 'Avenir', fontWeight: FontWeight.bold, color: greyColor, fontSize: 16);
+TextStyle profileLocStyle = new TextStyle (fontFamily: 'Avenir', fontWeight: FontWeight.bold, color: greyColor, fontSize: 13);
+TextStyle profileKpiStyle = new TextStyle (fontFamily: 'Avenir', fontWeight: FontWeight.bold, color: greyColor, fontSize: 10);
+//TextStyle profileLocStyle = new TextStyle (fontFamily: 'Avenir', fontWeight: FontWeight.bold, color: greyColor, fontSize: 14);
+
